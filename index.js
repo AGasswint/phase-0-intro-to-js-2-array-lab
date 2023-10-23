8d91c0299e9c0efd31b204b9f4ac6350e3020cfb
Anthony Gasswint <gasswint.a@gmail.com>
2023-10-22 19:45:57 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+
+let cats =["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
+}
+
+
+function appendCat(name) {
+    let copyOfCats = [...cats];
+    copyOfCats.push(name)
+    return copyOfCats
+}
+
+function prependCat(name) {
+    let copyOfCats = [...cats];
+    copyOfCats.unshift(name)
+    return copyOfCats
+}
+
+function removeLastCat(name) {
+    let copyOfCats = [...cats];
+    copyOfCats.pop(name)
+    return copyOfCats
+}
+
+function removeFirstCat(name) {
+    let copyOfCats = [...cats];
+    copyOfCats.shift(name)
+    return copyOfCats
+}
